b8c459838b2bc9523005ba186fc0ce7c5b86f471
Jimmy Baker <jimmyebaker@gmail.com>
2018-09-11 16:33:24 -0500

    Added requiresMainQueueSetup

--- ios/RNStarPrnt.m
+++ ios/RNStarPrnt.m
@@ -16,6 +16,11 @@
 
  bool hasListeners;
 
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_queue_create("net.infoxication.react.starprnt", DISPATCH_QUEUE_SERIAL);
