d723d67a637423c5cbf80d27df587567305956f7
dnlowman <dnlowman@gmail.com>
2020-02-04 11:44:25 +0000

    WIP: Added sample test

---
+++ android/src/test/java/net/infoxication/reactstarprnt/RNStarPrntModuleTest.java
@@ -0,0 +1,16 @@
+package net.infoxication.reactstarprnt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RNStarPrntModuleTest {
+    @Test
+    void example() {
+        // Given
+
+        // When
+
+        // Then
+        assertEquals(1, 2);
+    }
+}
